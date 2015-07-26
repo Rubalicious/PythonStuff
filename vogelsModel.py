8ac1a52d6a84e070f3adf903eb73a15d51fb844a
Rubalicious <rabrams12@email.arizona.edu>
2015-07-26 10:25:41 -0400

    uploading bst, primenumbers and random walks

--- vogelsModel.py
+++ vogelsModel.py
@@ -13,13 +13,22 @@ def fib(n):
         return fib(n-1)+fib(n-2)
 
 theta = 137.508
-a = 5
-b = 0.7
+a = 1
+b = 1
 c = 1
 #radius = c*theta**0.5
 
 for r in range(500):
     radius = a + b*r**(1/c)
+    red = r/500.0
+    gre = r/500.0
+    blu = r/500.0
+    if r%3 == 0:
+        bob.pencolor((1-red,1-gre,0))
+    elif r%3 == 1:
+        bob.pencolor((1,1,0))
+    else:
+        bob.pencolor((1,gre,blu))
     bob.dot()
     bob.penup()
     bob.left(theta)
