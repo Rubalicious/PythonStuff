191a2045eaf3654dc5dba548e19685994819d4bf
Rubalicious <rabrams12@email.arizona.edu>
2017-05-04 21:51:51 -0700

    updating repo

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,13 @@
+import sys
+
 def fib(n):
 	if n == 0:
-		return 1
+		return 0
 	if n == 1:
 		return 1
-	else:
-		return fib(n-1)+fib(n-2)
+	return fib(n-1)+fib(n-2)
 
-for i in range(31):
-	print "the %s th term of the fibonacci sequence is %s" %(i,fib(i))
+for i in range(int(sys.argv[1])):
+	# print "the %s th term of the fibonacci sequence is %s" %(i,fib(i))
+	print(str(fib(i))+" ",end='')
+print()
