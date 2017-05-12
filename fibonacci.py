21b02452237bb3bb95ae2c8e9364b532bb05bcab
Rubalicious <rabrams12@email.arizona.edu>
2017-05-11 22:38:49 -0700

    update

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,10 @@
 import sys
 
+# recursively calculates the nth
+# fibonacci element
 def fib(n):
 	if n == 0:
 		return 0
 	if n == 1:
 		return 1
 	return fib(n-1)+fib(n-2)
-
-for i in range(int(sys.argv[1])):
-	# print "the %s th term of the fibonacci sequence is %s" %(i,fib(i))
-	print(str(fib(i))+" ",end='')
-print()
